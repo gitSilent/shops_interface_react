43258f6acaeea460116b024f5f1a77701fb6da6b
gitSilent <97840080+gitSilent@users.noreply.github.com>
2022-10-20 19:29:52 +0300

    n

--- src/App.js
+++ src/App.js
@@ -3,10 +3,28 @@ import { Route, Router, BrowserRouter, Routes } from 'react-router-dom';
 import './App.css';
 import AuthorizationWindow from './AuthorizationWindow';
 import UserAccount from './UserAccount';
+import abi from './abi'
+import Web3 from 'web3';
 
 function App() {
+  let contractAddress, web3, contractInstance;
 
-  
+  const [accounts_arr, setAccountsArr] = React.useState([]);
+    
+
+      contractAddress = "0x8f9BBE8FfeF5A9bE18C4981821EEe7B7811D9bd4";
+      web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+      console.log(web3);
+      contractInstance = new web3.eth.Contract(abi, contractAddress);
+      
+      React.useEffect(()=>{
+        web3.eth.getAccounts()
+        .then((val)=>{
+          setAccountsArr(val)
+
+          console.log(accounts_arr)
+        })
+      },[])
 
   //https://stackoverflow.com/questions/34735580/how-to-do-a-redirect-to-another-route-with-react-router
   return (
@@ -15,7 +33,12 @@ function App() {
         <div className="App">
         
           <Routes>
-            <Route path='/' element={<AuthorizationWindow />} />
+            <Route path='/' element={<AuthorizationWindow 
+            contractAddress={contractAddress}
+            web3={web3}
+            contractInstance={contractInstance}
+            accounts_arr={accounts_arr}
+             />} />
             <Route path='/account' element={<UserAccount />} />
               
           </Routes>
--- src/AuthorizationWindow/AuthorizationWindow.jsx
+++ src/AuthorizationWindow/AuthorizationWindow.jsx
@@ -2,35 +2,55 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import classes from './AuthorizationWindow.module.css'
 
-function AuthorizationWindow(){
+function AuthorizationWindow(props){
+let current_user_address;
+
+const [loginInputValue, setLogin] = React.useState();
+const [arrAccounts, setAccounts] = React.useState([]);
 
     let navigate = useNavigate()
     console.log(navigate)
 
-    function onSubmitEvent(event){
+    function enterAccount(event){
         event.preventDefault();
-        // const history = useHistory()
-        // console.log(history)
-        console.log('event')
-        global.contractInstance = "instance"
-        console.log(global.contractInstance)
+
 
         navigate('/account')
-
-
-       
     }
+    console.log(props)
+    
+    // setAccounts(props.accounts_arr)
+    React.useEffect(()=>{
+        (()=>{
+            console.log(props.accounts_arr)
+            setAccounts(props.accounts_arr);
+            console.log(arrAccounts)
+
+        })()
+
+        // console.log(props.accounts_arr)
+    },[])
+
     return(
         <div className={classes.authorization_window}>
-            <form className={classes.auth_form} onSubmit={onSubmitEvent}>
+            <form className={classes.auth_form} onSubmit={enterAccount}>
                 <h2 className="h2-auth">Авторизация</h2>
-                <select name="" id="" className="select-auth" >
-                    <option value="">user address</option>
+                <select name="" id="" className="select-auth" onChange={(event)=>{
+                    let selected_option = event.target.closest("select").selectedIndex;
+                    setLogin(selected_option);
+                }}>
+                     {
+                        arrAccounts.map((el)=>(
+                            <option>{el}</option>
+                        ))
+                    }
+                   
                 </select>
                 <input
                 type="text"
                 className="auth-login-input"
                 placeholder="Enter your login"
+                value={loginInputValue}
                 />
                 <input
                 type="text"
