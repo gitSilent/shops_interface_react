b4d7a0b19ecb81f3e85e834a8e15b3b64b70167f
gitSilent <97840080+gitSilent@users.noreply.github.com>
2022-10-30 18:42:22 +0300

    start_blockchain_bat

--- src/App.js
+++ src/App.js
@@ -15,10 +15,19 @@ function App() {
   
   let contractAddress, web3, contractInstance;    
 
-  contractAddress = "0x9B23362aBdc2C08Dd36391d67dD5bb702D596040";
-  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:7545"));
+  contractAddress = "0xFE194952c05b982cA9d9B57B32C6744655a3A4c0";
+  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
   console.log(web3);
   contractInstance = new web3.eth.Contract(abi, contractAddress);
+
+  function unlockAccounts(){
+    web3.eth.personal.unlockAccount("0x66B667951A9AB362dfdcB7521912eD2b34fE8c02", "1234", 0)
+    web3.eth.personal.unlockAccount("0x859A98018Ab085707a207968E618060B5F479Bc8", "1234", 0)
+    web3.eth.personal.unlockAccount("0xc691Cf57258CcA6CFC3863f011802034c58dC660", "1234", 0)
+    web3.eth.personal.unlockAccount("0x5891d4fb3a23DC213BA7B38d0420Be66448e8658", "1234", 0)
+    web3.eth.personal.unlockAccount("0xAaD48Ed4104D89Cd5C77a01A0912A6BF9B3Ba775", "1234", 0)
+    web3.eth.personal.unlockAccount("0xE96Be8b02Ed442a50dC4B44Eed9a2CC64de740A3", "1234", 0)
+  }
 
   dispatch(setContractAddress(contractAddress))
   
@@ -40,6 +49,8 @@ function App() {
       console.log(reg_users_arr)
      
     })
+
+    unlockAccounts()
     
   },[])
 
